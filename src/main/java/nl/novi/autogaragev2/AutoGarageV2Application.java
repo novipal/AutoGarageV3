3a1645c102ff4b391f0acca5f224103c3aeb093e
novipal <serkan.palaz@novi-education.nl>
2022-12-23 15:49:34 +0100

    Initial commit

---
+++ src/main/java/nl/novi/autogaragev2/AutoGarageV2Application.java
@@ -0,0 +1,13 @@
+package nl.novi.autogaragev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGarageV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoGarageV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/autogaragev2/AutoGarageV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.autogaragev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoGarageV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
